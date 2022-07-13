ba16e945da7279513be29df4f3c2116cf9f6fe39
mcseemz <mcseemz@gmail.com>
2022-07-13 18:35:33 +0200

    Initial application draft

---
+++ src/main/java/com/mcseemz/diner/HeroesDinerApplication.java
@@ -0,0 +1,13 @@
+package com.mcseemz.diner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesDinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeroesDinerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mcseemz/diner/HeroesDinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcseemz.diner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesDinerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
